548518f079298b822a6f127f3b5a724587e09099
Bobby Richter <robert.c.richter@gmail.com>
2017-10-22 15:27:18 -0500

    Remove unused dependencies

--- project.clj
+++ project.clj
@@ -4,6 +4,5 @@
   :license {:name "GNU General Public License"
             :url "http://www.gnu.org/licenses/gpl.txt"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/test.check "0.9.0"]
-                 [instaparse "1.4.8"]])
+                 [org.clojure/test.check "0.9.0"]])
 
