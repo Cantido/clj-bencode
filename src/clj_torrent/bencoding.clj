98d27c9912073cc4627a487901baee1321e2d9cf
Bobby Richter <robert.c.richter@gmail.com>
2017-10-22 15:14:32 -0500

    Encode and decode dicts

--- src/clj_torrent/bencoding.clj
+++ src/clj_torrent/bencoding.clj
@@ -1,24 +1,25 @@
 (ns clj-torrent.bencoding
-  (:require [instaparse.core :as insta])
-  (:require [instaparse.transform :as transform])
-  (:import (java.util Collection)
+  (:import (java.util Collection Map)
            (java.nio.charset StandardCharsets)
-           (java.nio ByteBuffer)))
+           (clojure.lang MapEntry Keyword)))
 
-(defn to-utf8 [^String x]
+(defn- to-utf8 [^String x]
   (.encode StandardCharsets/UTF_8 x))
 
-(defn from-utf8 [^String x]
+(defn- from-utf8 [^String x]
   (str (.decode StandardCharsets/UTF_8 x)))
 
-(defmulti represent class)
+(defmulti represent class :default String)
 
-(defmethod represent Long [x] (str "i" x "e"))
-(defmethod represent String [x] (str (count x) ":" x))
-(defmethod represent Collection [x] (str "l" (apply str (map represent x)) "e"))
+(defmethod represent Long [x] ["i" x "e"])
+(defmethod represent String [x] [(count x) ":" x])
+(defmethod represent Keyword [x] (represent (name x)))
+(defmethod represent Collection [x] (flatten ["l" (map represent x) "e"]))
+(defmethod represent Map [x] (flatten ["d" (map represent (.entrySet x)) "e"]))
+(defmethod represent MapEntry [x] [(represent (key x)) (represent (val x))])
 
 (defn encode [x]
-  (to-utf8 (represent x)))
+  (to-utf8 (apply str (represent x))))
 
 (defn- numeric? [c]
   (#{\1 \2 \3 \4 \5 \6 \7 \8 \9 \0} c))
@@ -53,13 +54,30 @@
     (assert (= \l (first tag-char)))
     (assert (= \e (first end-tag)))
     [list-contents
-     after-contents]))
+     after-end]))
+
+(defn map-every-nth
+  ([f coll n] (map-every-nth f coll n 0))
+  ([f coll n offset]
+   (map-indexed
+     #(if (zero? (mod (inc (- %1 offset)) n)) (f %2) %2)
+     coll)))
+
+(defn- split-next-dict [x]
+  (let [[tag-char after-tag] (split-at 1 x)
+        [list-contents after-contents] (extract-list-contents [] after-tag)
+        [end-tag after-end] (split-at 1 after-contents)]
+    (assert (= \d (first tag-char)))
+    (assert (= \e (first end-tag)))
+    [(apply hash-map (map-every-nth keyword list-contents 2 1))
+     after-end]))
 
 (defn- split-next [x]
   (cond
-    (= \i (first x)) (split-next-int x)
     (numeric? (first x)) (split-next-string x)
-    (= \l (first x)) (split-next-list x)))
+    (= \i (first x)) (split-next-int x)
+    (= \l (first x)) (split-next-list x)
+    (= \d (first x)) (split-next-dict x)))
 
 (defn- extract-list-contents [coll s]
   (if (not= \e (first s))
--- test/clj_torrent/bencoding_test.clj
+++ test/clj_torrent/bencoding_test.clj
@@ -10,6 +10,9 @@
 (defn utf8 [^String x]
   (.encode StandardCharsets/UTF_8 x))
 
+(defn utf8d [x]
+  (str (.decode StandardCharsets/UTF_8 x)))
+
 (deftest encode-test
   (testing "encode an integer"
     (is (= (utf8 "i3e") (b/encode 3)))
@@ -27,7 +30,14 @@
       (is (= (utf8 "2:𐐷") (b/encode "𐐷")))
       (is (= (utf8 "4:𐐷bc") (b/encode "𐐷bc")))
       (is (= (utf8 "4:a𐐷c") (b/encode "a𐐷c")))
-      (is (= (utf8 "4:ab𐐷") (b/encode "ab𐐷"))))))
+      (is (= (utf8 "4:ab𐐷") (b/encode "ab𐐷")))))
+  (testing "encode a dict"
+    (testing "that is empty"
+      (is (= "de") (utf8d (b/encode {}))))
+    (testing "that contains string keys and values"
+      (is (= "d3:cow3:mooe" (utf8d (b/encode {:cow "moo"}))))
+      (is (= "d3:cow3:moo4:spam4:eggse" (utf8d (b/encode {:cow "moo" :spam "eggs"})))))))
+
 
 (deftest decode-test
   (testing "decode an integer"
@@ -45,11 +55,24 @@
     (is (= [1 2 3] (b/decode (utf8 "li1ei2ei3ee")))))
   (testing "decode a list of strings"
     (is (= ["a" "b" "c"] (b/decode (utf8 "l1:a1:b1:ce"))))
-    (is (= ["0" ""] (b/decode (utf8 "l1:00:e"))))))
-  ;(testing "decode a dict"
-  ;  (is (= {:cow "moo" :spam "eggs"} (b/decode "d3:cow3:moo4:spam4:eggse")))))
+    (is (= ["0" ""] (b/decode (utf8 "l1:00:e")))))
+  (testing "decode a mixed list"
+    (is (= [{} 0] (b/decode (utf8 "ldei0ee")))))
+  (testing "decode a dict"
+    (is (= {:cow "moo" :spam "eggs"} (b/decode (utf8 "d3:cow3:moo4:spam4:eggse"))))))
 
+(def gen-primitives (gen/one-of [gen/int gen/string]))
+(def gen-list (gen/list gen-primitives))
+(def gen-dict (gen/map gen/keyword gen-primitives))
+(def gen-leaf (gen/one-of [gen-primitives gen-list gen-dict]))
 
+;; Just to make an editor shut up about these defspec's
+(declare
+  x
+  encode-ints encode-strings
+  encode-int-lists encode-string-lists encode-mixed-lists
+  encode-string-dicts encode-int-dicts encode-mixed-dicts
+  encode-mixed-nested-dicts encode-mixed-nested-lists)
 
 (defspec encode-ints 1000
   (prop/for-all [x gen/int]
@@ -59,10 +82,34 @@
   (prop/for-all [x gen/string]
     (= x (-> x b/encode b/decode))))
 
-(defspec encode-int-lists 1000
-         (prop/for-all [x (gen/list gen/int)]
-                       (= x (-> x b/encode b/decode))))
-;
-(defspec encode-string-lists 1000
+(defspec encode-int-lists
+  (prop/for-all [x (gen/list gen/int)]
+    (= x (-> x b/encode b/decode))))
+
+(defspec encode-string-lists
   (prop/for-all [x (gen/list gen/string)]
     (= x (-> x b/encode b/decode))))
+
+;; the complexity of mixed collections balloons rapidly,
+;; so setting this much higher than 50 tests will
+;; take way too long
+(defspec encode-mixed-lists 50
+  (prop/for-all [x (gen/list gen-leaf)]
+    (= x (-> x b/encode b/decode))))
+
+(defspec encode-string-dicts
+  (prop/for-all [x (gen/map gen/keyword gen/string)]
+    (= x (-> x b/encode b/decode))))
+
+(defspec encode-int-dicts
+  (prop/for-all [x (gen/map gen/keyword gen/int)]
+    (= x (-> x b/encode b/decode))))
+
+;; the complexity of mixed collections balloons rapidly,
+;; so setting this much higher than 50 tests will
+;; take way too long
+(defspec encode-mixed-dicts 50
+  (prop/for-all [x (gen/map gen/keyword gen-leaf)]
+    (= x (-> x b/encode b/decode))))
+
+
