a530690d149cc2426bee1680c9d6d8353a987241
Bobby Richter <robert.c.richter@gmail.com>
2017-10-22 15:52:43 -0500

    Add deploy repository

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/Cantido/clj-bencode"
   :license {:name "GNU General Public License"
             :url "http://www.gnu.org/licenses/gpl.txt"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/test.check "0.9.0"]])
 
