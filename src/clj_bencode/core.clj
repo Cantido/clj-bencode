298ac522574507b55383c17b3ad06bc6f8c1248b
Bobby Richter <robert.c.richter@gmail.com>
2017-10-22 22:47:15 -0500

    Make predicates return real booleans

--- src/clj_bencode/core.clj
+++ src/clj_bencode/core.clj
@@ -42,10 +42,10 @@
   (to-utf8 (apply str (represent x))))
 
 (defn- numeric? [c]
-  (#{\1 \2 \3 \4 \5 \6 \7 \8 \9 \0} c))
+  (boolean (#{\1 \2 \3 \4 \5 \6 \7 \8 \9 \0} c)))
 
 (defn- numeric-or-sign? [c]
-  (#{\1 \2 \3 \4 \5 \6 \7 \8 \9 \0 \-} c))
+  (boolean (#{\1 \2 \3 \4 \5 \6 \7 \8 \9 \0 \-} c)))
 
 (defn- split-next-int [x]
   (let [[tag-char after-tag] (split-at 1 x)
