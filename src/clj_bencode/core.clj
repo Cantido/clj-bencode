b22798022fcae02b16ad98d068c997ee26a34753
Bobby Richter <robert.c.richter@gmail.com>
2017-10-25 23:36:20 -0500

    Actually handle unicode properly
    A problem with b-encoding is that byte strings are used both
    as text strings and as buckets for binary data. That becomes
    a problem when you're trying to parse them as strings.
    
    I think this change is the ideal compromise: if a string is pure ASCII,
    it's decoded as a string. If not, it's left totally alone. That leaves
    the unicode-decoding totally up to the consumer, which is appropriate,
    and no bytes are ever modified.
    
    Strings to get encoded as UTF-8, but that's common practice.

--- src/clj_bencode/core.clj
+++ src/clj_bencode/core.clj
@@ -9,92 +9,73 @@
 
 (def ^:private byte-array-class (class (byte-array 1)))
 
-(defmulti ^:private  byte-buffer
-  "Coerces the arg into a java.nio.ByteBuffer"
-  class)
+(defn- int-base-10 [x]
+  (->> x int str seq (map byte) byte-array))
 
-(defmethod byte-buffer ByteBuffer [x] x)
-(defmethod byte-buffer byte-array-class [x] (ByteBuffer/wrap x))
+(declare ^:private represent)
 
-(defn- bb-array
-  "Extracts the contents of the byte buffer, truncating unused buffer space.
-   Hides gross mutability."
-  [^ByteBuffer b]
-  (let [arr (byte-array (.limit b))]
-    (.get b arr)
-    arr))
-
-(defn- cb-array
-  [^CharBuffer b]
-  (let [arr (char-array (.limit b))]
-    (.get b arr)
-    arr))
-
-(defn to-utf8
-  "Encodes a string into a UTF-8 byte array."
-  [& more]
-  (.getBytes (apply str more) StandardCharsets/UTF_8))
-
-(defn- stringify
-  "Stringifies then byteifies an integer."
+(defn- represent-integer
+  "Converts the given integer into a b-encoded byte array."
   [x]
-  (byte-array (map byte (seq (str x)))))
-
-
-(defn from-utf8
-  "Decodes the given byte array into a string, assuming it is UTF-8-encoded."
-  ^String [x]
-  (String. (bytes x) StandardCharsets/UTF_8))
-
-(defmulti ^:private ^ByteArrayOutputStream represent class :default String)
-
-(defn- represent-long
-  ^ByteArrayOutputStream [x]
-  (doto (ByteArrayOutputStream.)
-    (.write (byte \i))
-    (.write (byte-array (map byte (seq (str x)))))
-    (.write (byte \e))))
-
-(defn represent-seq
-  ^ByteArrayOutputStream [s]
-  (doto (ByteArrayOutputStream.)
-    (.write (byte-array (mapcat (comp #(.toByteArray %) represent) s)))))
-
-(defn- represent-collection
-  ^ByteArrayOutputStream [coll]
-  (doto (ByteArrayOutputStream.)
-    (.write (byte \l))
-    (.write (.toByteArray (represent-seq coll)))
-    (.write (byte \e))))
-
-
-(defn- represent-map ^ByteArrayOutputStream [m]
-  (doto (ByteArrayOutputStream.)
-    (.write (byte \d))
-    (.write (.toByteArray (represent-seq (apply concat (sort (seq m))))))
-    (.write (byte \e))))
-
-(defn- represent-string
-  ^ByteArrayOutputStream [s]
-  (let [encoded-str (.getBytes (str s) StandardCharsets/UTF_8)]
+  (.toByteArray
     (doto (ByteArrayOutputStream.)
-      (.write (bytes (stringify (count encoded-str))))
-      (.write (byte \:))
-      (.write (bytes encoded-str)))))
+      (.write (byte \i))
+      (.write (byte-array (map byte (seq (str (bigint x))))))
+      (.write (byte \e)))))
 
-(defmethod represent Number [x] (represent-long x))
+(defn- represent-seq
+  "Converts the given sequence into a byte array.
+   Not the same as represent-collection; this function does
+   not put characters at the start and end of the array."
+  [s]
+  (.toByteArray
+    (doto (ByteArrayOutputStream.)
+      (.write (byte-array (mapcat represent (seq s)))))))
+
+(defn- represent-collection [coll]
+  (.toByteArray
+    (doto (ByteArrayOutputStream.)
+      (.write (byte \l))
+      (.write (bytes (represent-seq (seq coll))))
+      (.write (byte \e)))))
+
+(defn- represent-map [m]
+  (.toByteArray
+    (doto (ByteArrayOutputStream.)
+      (.write (byte \d))
+      (.write (bytes (represent-seq (apply concat (sort (seq m))))))
+      (.write (byte \e)))))
+
+(defn- represent-bytes [xs]
+  (.toByteArray
+    (doto (ByteArrayOutputStream.)
+      (.write (bytes (int-base-10 (count xs))))
+      (.write (byte \:))
+      (.write (bytes xs)))))
+
+(defn- represent-string [s]
+  (represent-bytes (.getBytes (str s) StandardCharsets/UTF_8)))
+
+(defmulti ^:private represent class :default String)
+
+(defmethod represent Number [x] (represent-integer x))
 (defmethod represent String [x] (represent-string x))
+(defmethod represent byte-array-class [x] (represent-bytes x))
 (defmethod represent Keyword [x] (represent (name x)))
 (defmethod represent Collection [x] (represent-collection x))
 (defmethod represent Map [x] (represent-map x))
 
 
-(defn encode [x]
-  (.toByteArray (represent x)))
+(defn encode
+  "Encodes x into a b-encoded byte array.
+
+   Strings are encoded to UTF-8, and byte arrays are stored untouched."
+  [x]
+  (represent x))
 
 
 
-(declare decode-next)
+(declare ^:private decode-next)
 
 (defn- decode-int [stream delimeter & ch]
   (loop [i (if (nil? ch) (.read stream) (first ch)), result ""]
@@ -103,13 +84,20 @@
         (BigInteger. result)
         (recur (.read stream) (str result c))))))
 
-(defn- decode-string
+(defn- in-ascii-range? [x] (<= 0 (int x) 127))
+
+(defn- ascii? [xs]
+  (every? in-ascii-range? (seq xs)))
+
+(defn- decode-bytes
   [^InputStream rdr start]
   {:pre [(<= 0 (int start) 255)]}
   (let [length (decode-int rdr \: start)
-        buffer (make-array Byte/TYPE length)]
+        buffer (bytes (make-array Byte/TYPE length))]
     (.read rdr (bytes buffer))
-    (String. (bytes buffer) StandardCharsets/UTF_8)))
+    (if (ascii? buffer)
+      (String. buffer StandardCharsets/US_ASCII)
+      buffer)))
 
 (defn- decode-list
   [^InputStream rdr]
@@ -120,18 +108,16 @@
         (recur (conj result (decode-next rdr (int c))))))))
 
 (defn- decode-dict
-  "Splits the next value off of x and interprets it as a map, and returns
-   [({\"key\" val}) (rest-of-coll)]."
   [^InputStream rdr]
   (apply hash-map (decode-list rdr)))
 
 (defn- numeric? [c]
   (boolean (#{\1 \2 \3 \4 \5 \6 \7 \8 \9 \0} (char c))))
 
-(defn decode-next [stream & i]
+(defn- decode-next [stream & i]
   (let [indicator (if (nil? i) (.read stream) (first i))]
     (cond
-      (numeric? indicator) (decode-string stream indicator)
+      (numeric? indicator) (decode-bytes stream indicator)
       (= (char indicator) \i) (decode-int stream \e)
       (= (char indicator) \l) (decode-list stream)
       (= (char indicator) \d) (decode-dict stream))))
@@ -140,6 +126,7 @@
   "Decodes b-encoded data.
 
   Accepts any data type which can satisfy clojure.java.io/IOFactory,
-  which includes files, input streams, bytes arrays, and strings."
+  which includes files, input streams, bytes arrays, and
+  strings (which are assumed to be URIs, then file paths if that won't work)."
   [x]
   (-> x io/input-stream decode-next))
--- test/clj_bencode/core_test.clj
+++ test/clj_bencode/core_test.clj
@@ -11,17 +11,15 @@
 
 (def truncated-file-url (io/resource "linuxmint-18.2-cinnamon-64bit.iso.torrent-test"))
 (def full-file-url (io/resource "linuxmint-18.2-cinnamon-64bit.iso.torrent"))
-(def bytes-file-url (io/resource "grosspart.bin"))
 
 (def torrentstring "d8:announce43:https://torrents.linuxmint.com/announce.php10:created by25:Transmission/2.84 (14307)13:creation datei1499021259e8:encoding5:UTF-84:infod6:lengthi1676083200e4:name33:linuxmint-18.2-cinnamon-64bit.iso12:piece lengthi1048576e6:pieces1:a7:privatei0eee")
 
 (defn strbytes [x] (IOUtils/toByteArray (str x)))
-(defn filebytes [^URL x] (IOUtils/toByteArray x))
-
-(def endec (comp b/decode b/encode))
-(def bytestr (comp str char int))
-
-(def weird-str (bytestr 128))
+(defn filebytes [^URL x] (let [file (io/file x)
+                               length (.length file)
+                               buf (byte-array length)]
+                           (IOUtils/readFully (io/input-stream x) buf)
+                           buf))
 
 (deftest decode-real-string-test
   (let [result (b/decode (strbytes torrentstring))]
@@ -59,102 +57,91 @@
       (is (= 1676083200 (get info "length")))
       (is (= 1048576 (get info "piece length")))
       (is (= "linuxmint-18.2-cinnamon-64bit.iso" (get info "name")))
-      (is (= 31980 (count
-                     (.getBytes
-                      (str (get info "pieces"))
-                      StandardCharsets/UTF_8))))
-      (is (= 0 (get info "private"))))))
+      (is (= 0 (get info "private")))
+      (let [pieces (get info "pieces")]
+        (is (= 31980 (count pieces)))))))
+
 
 (deftest encode-full-file-test
   (let [file (filebytes full-file-url)
         decoded (b/decode file)
         encoded (b/encode decoded)]
-    (spit (io/file "decoded") (str decoded))
-    (spit (io/file "reencoded") (String. encoded))
-    (is (= (count (seq file))
-           (count (seq encoded))))))
-
-(deftest bytes-file-unicode-test
-  (let [file (filebytes bytes-file-url)
-        decoded (b/from-utf8 file)
-        encoded (b/to-utf8 decoded)]
-    (is (= (count (seq file))
+    (is (= 32244
+           (count (seq file))
            (count (seq encoded))))))
 
 (deftest encode-test
   (testing "encode"
     (testing "integers"
-      (is (= "i3e" (b/from-utf8 (b/encode 3))))
-      (is (= "i0e" (b/from-utf8 (b/encode 0))))
-      (is (= "i-1e" (b/from-utf8 (b/encode -1)))))
+      (is (= "i3e" (String. (bytes (b/encode 3)))))
+      (is (= "i0e" (String. (bytes (b/encode 0)))))
+      (is (= "i-1e" (String. (bytes (b/encode -1))))))
     (testing "strings"
-      (is (= "1:a" (b/from-utf8 (b/encode "a"))))
-      (is (= "3:foo" (b/from-utf8 (b/encode "foo"))))
-      (is (= "7:foo bar" (b/from-utf8 (b/encode "foo bar"))))
-      (is (= (str "2:" weird-str) (b/from-utf8 (b/encode weird-str))))
+      (is (= "1:a" (String. (bytes (b/encode "a")))))
+      (is (= "3:foo" (String. (bytes (b/encode "foo")))))
+      (is (= "7:foo bar" (String. (bytes (b/encode "foo bar")))))
       (testing "containing characters above U+FFFF"
-        (is (= "4:𐐷" (b/from-utf8 (b/encode "𐐷"))))
-        (is (= "6:𐐷bc" (b/from-utf8 (b/encode "𐐷bc"))))
-        (is (= "6:a𐐷c" (b/from-utf8 (b/encode "a𐐷c"))))
-        (is (= "6:ab𐐷" (b/from-utf8 (b/encode "ab𐐷"))))))
+        ; "\uD801\uDC37")
+        (is (= "4:𐐷" (String. (bytes (b/encode "𐐷")))))
+        (is (= "6:𐐷bc" (String. (bytes (b/encode "𐐷bc")))))
+        (is (= "6:a𐐷c" (String. (bytes (b/encode "a𐐷c")))))
+        (is (= "6:ab𐐷" (String. (bytes (b/encode "ab𐐷")))))))
     (testing "lists"
       (testing "of integers"
-        (is (= "li1ei2ei3ee" (b/from-utf8 (b/encode [1 2 3])))))
+        (is (= "li1ei2ei3ee" (String. (bytes (b/encode [1 2 3]))))))
       (testing "of strings"
-        (is (= "l1:a1:b1:ce" (b/from-utf8 (b/encode ["a" "b" "c"]))))
-        (is (= "l1:00:e" (b/from-utf8 (b/encode ["0" ""])))))
+        (is (= "l1:a1:b1:ce" (String. (bytes (b/encode ["a" "b" "c"])))))
+        (is (= "l1:00:e" (String. (bytes (b/encode ["0" ""]))))))
       (testing "in edge cases"
-        (is (= "le" (b/from-utf8 (b/encode []))))
-        (is (= "llee" (b/from-utf8 (b/encode [[]]))))))
+        (is (= "le" (String. (bytes (b/encode [])))))
+        (is (= "llee" (String. (bytes (b/encode [[]])))))))
     (testing "maps"
       (testing "that are empty"
-        (is (= "de" (b/from-utf8 (b/encode {})))))
+        (is (= "de" (String. (bytes (b/encode {}))))))
       (testing "that contains string keys and values"
-        (is (= "d0:lee" (b/from-utf8 (b/encode {"" []}))))
-        (is (= "d3:cow3:mooe" (b/from-utf8 (b/encode {:cow "moo"}))))
-        (is (= "d8:cow says3:mooe" (b/from-utf8 (b/encode {"cow says" "moo"}))))
-        (is (= "d3:cow3:moo4:spam4:eggse" (b/from-utf8 (b/encode {"cow" "moo" "spam" "eggs"})))))
+        (is (= "d0:lee" (String. (bytes (b/encode {"" []})))))
+        (is (= "d3:cow3:mooe" (String. (bytes (b/encode {:cow "moo"})))))
+        (is (= "d8:cow says3:mooe" (String. (bytes (b/encode {"cow says" "moo"})))))
+        (is (= "d3:cow3:moo4:spam4:eggse" (String. (bytes (b/encode {"cow" "moo" "spam" "eggs"}))))))
       (testing "should sort their keys"
-        (is (= "d1:ai1e1:bi2ee" (b/from-utf8 (b/encode {"b" 2 "a" 1}))))))))
+        (is (= "d1:ai1e1:bi2ee" (String. (bytes (b/encode {"b" 2 "a" 1})))))))))
+
+(defn byte-seq [s]
+  (seq (.getBytes (str s))))
 
 (deftest decode-test
   (testing "decode"
     (testing "integers"
-      (is (= 1 (b/decode (b/to-utf8 "i1e"))))
-      (is (= 10 (b/decode (b/to-utf8 "i10e"))))
-      (is (= -10 (b/decode (b/to-utf8 "i-10e")))))
+      (is (= 1 (b/decode (.getBytes "i1e"))))
+      (is (= 10 (b/decode (.getBytes  "i10e"))))
+      (is (= -10 (b/decode (.getBytes  "i-10e")))))
     (testing "strings"
-      (is (= "foo" (b/decode (b/to-utf8 "3:foo"))))
-      (is (= "foo bar" (b/decode (b/to-utf8 "7:foo bar"))))
-      (is (=  weird-str (b/decode (b/to-utf8 (str "2:" weird-str)))))
+      (is (= "foo" (b/decode (.getBytes  "3:foo"))))
+      (is (= "foo bar" (b/decode (.getBytes  "7:foo bar"))))
       (testing "including characters above U+FFFF"
-        (is (= "𐐷" (b/decode (b/to-utf8 "4:𐐷"))))
-        (is (= "𐐷bc" (b/decode (b/to-utf8 "6:𐐷bc"))))
-        (is (= "a𐐷c" (b/decode (b/to-utf8 "6:a𐐷c"))))
-        (is (= "ab𐐷" (b/decode (b/to-utf8 "6:ab𐐷"))))))
+        (is (= (byte-seq "𐐷") (seq (b/decode (.getBytes  "4:𐐷")))))
+        (is (= (byte-seq "𐐷bc") (seq (b/decode (.getBytes  "6:𐐷bc")))))
+        (is (= (byte-seq "a𐐷c") (seq (b/decode (.getBytes  "6:a𐐷c")))))
+        (is (= (byte-seq "ab𐐷") (seq (b/decode (.getBytes  "6:ab𐐷")))))))
     (testing "lists"
-      (is (= [] (b/decode (b/to-utf8 "le"))))
-      (is (= [1] (b/decode (b/to-utf8 "li1ee"))))
+      (is (= [] (b/decode (.getBytes  "le"))))
+      (is (= [1] (b/decode (.getBytes  "li1ee"))))
       (testing "of integers"
-        (is (= [1 2 3] (b/decode (b/to-utf8 "li1ei2ei3ee")))))
+        (is (= [1 2 3] (b/decode (.getBytes  "li1ei2ei3ee")))))
       (testing "of strings"
-        (is (= ["a" "b" "c"] (b/decode (b/to-utf8 "l1:a1:b1:ce"))))
-        (is (= ["0" ""] (b/decode (b/to-utf8 "l1:00:e")))))
+        (is (= ["a" "b" "c"] (b/decode (.getBytes  "l1:a1:b1:ce"))))
+        (is (= ["0" ""] (b/decode (.getBytes  "l1:00:e")))))
       (testing "of mixed contents"
-        (is (= [{} 0] (b/decode (b/to-utf8 "ldei0ee"))))))
+        (is (= [{} 0] (b/decode (.getBytes  "ldei0ee"))))))
     (testing "decode a dict"
-      (is (= {"" []} (b/decode (b/to-utf8 "d0:lee"))))
-      (is (= {"cow" "moo" "spam" "eggs"} (b/decode (b/to-utf8 "d3:cow3:moo4:spam4:eggse"))))
-      (is (= {"cow says" "moo" "spam" "eggs"} (b/decode (b/to-utf8 "d8:cow says3:moo4:spam4:eggse")))))))
+      (is (= {"" []} (b/decode (.getBytes  "d0:lee"))))
+      (is (= {"cow" "moo" "spam" "eggs"} (b/decode (.getBytes  "d3:cow3:moo4:spam4:eggse"))))
+      (is (= {"cow says" "moo" "spam" "eggs"} (b/decode (.getBytes  "d8:cow says3:moo4:spam4:eggse")))))))
 
 
-(deftest encode-decode-test
-  (is (= weird-str (endec weird-str)))
-  (is (= {(-> 129 char str) {"a" 1}} (b/decode (b/encode {(-> 129 char str) {"a" 1}})))))
-
-(def gen-primitives (gen/one-of [gen/int gen/string]))
+(def gen-primitives (gen/one-of [gen/int gen/string-ascii]))
 (def gen-list (gen/list gen-primitives))
-(def gen-dict (gen/map gen/string gen-primitives))
+(def gen-dict (gen/map gen/string-ascii gen-primitives))
 (def gen-leaf (gen/one-of [gen-primitives gen-list gen-dict]))
 
 ;; Just to make an editor shut up about these defspec's
@@ -167,16 +154,12 @@
   encode-mixed-nested-dicts encode-mixed-nested-lists
   unicode-handling)
 
-(defspec unicode-handling 1000
-  (prop/for-all [x gen/string]
-    (= x (-> x b/to-utf8 b/from-utf8))))
-
 (defspec encode-ints
          (prop/for-all [x gen/int]
                        (= x (-> x b/encode b/decode))))
 
 (defspec encode-strings
-         (prop/for-all [x gen/string]
+         (prop/for-all [x gen/string-ascii]
                        (= x (-> x b/encode b/decode))))
 
 (defspec encode-int-lists
@@ -184,7 +167,7 @@
                        (= x (-> x b/encode b/decode))))
 
 (defspec encode-string-lists
-         (prop/for-all [x (gen/list gen/string)]
+         (prop/for-all [x (gen/list gen/string-ascii)]
                        (= x (-> x b/encode b/decode))))
 
 (defspec encode-mixed-lists
@@ -192,22 +175,22 @@
                        (= x (-> x b/encode b/decode))))
 
 (defspec encode-shallow-dicts
-         (prop/for-all [x (gen/map gen/string-alphanumeric (gen/return []))]
+         (prop/for-all [x (gen/map gen/string-ascii (gen/return []))]
                        (= x (-> x b/encode b/decode))))
 
 (defspec encode-string-dicts
-         (prop/for-all [x (gen/map gen/string gen/string)]
+         (prop/for-all [x (gen/map gen/string-ascii gen/string-ascii)]
                        (= x (-> x b/encode b/decode))))
 
 (defspec encode-int-dicts
-  (prop/for-all [x (gen/map gen/string gen/int)]
+  (prop/for-all [x (gen/map gen/string-ascii gen/int)]
     (= x (-> x b/encode b/decode))))
 
 
-(defspec encode-dict-in-dict 50
-  (prop/for-all [x (gen/map gen/string (gen/map gen/string gen/int))]
+(defspec encode-dict-in-dict
+  (prop/for-all [x (gen/map gen/string-ascii (gen/map gen/string-ascii gen/int))]
     (= x (-> x b/encode b/decode))))
 
 (defspec encode-mixed-dicts
-  (prop/for-all [x (gen/map gen/string (gen/return {"a" 1}))]
+  (prop/for-all [x (gen/map gen/string-ascii (gen/return {"a" 1}))]
     (= x (-> x b/encode b/decode))))
